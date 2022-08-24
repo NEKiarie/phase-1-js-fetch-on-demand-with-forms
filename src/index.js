157c8fc35d9ded74eaa19348a9435878c849af0b
Elvis <s49188705@gmail.com>
2022-08-24 16:11:54 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
-}
+    //
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    //event.target.children[1].value
+    //console.log(event);
 
-document.addEventListener('DOMContentLoaded', init);
+    // Access Input Value Directly
+    const input = document.querySelector("input#searchByID");
+    //console.log(input.value);
+
+    //Fetch Data Based on User Input
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+
+      //Display Fetched Data on the Page
+      .then(data => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
